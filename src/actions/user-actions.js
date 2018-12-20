4a56481586e211f2210639f40801cd19b6749bc3
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-20 15:06:12 -0700

    update user action pseudocode

--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,20 +1,40 @@
-export const CREATE_USER = 'CREATE_USER';
-export const LOG_IN = 'LOG_IN';
+export const REQUEST = 'REQUEST';
+export const AUTH_REQUEST = 'AUTH_REQUEST';
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const LOG_OUT = 'LOG_OUT';
+export const SIGN_UP = 'SIGN_UP';
+export const LOG_IN = 'LOG_IN';
 
-export const createUser = (username, password, name, email) => ({
-    type: CREATE_USER,
-    username,
-    password,
-    name,
-    email
+
+export const request = () => ({
+    type: REQUEST
+});
+
+export const authRequest = () => ({
+    type: AUTH_REQUEST
+});
+
+export const setAuthToken = authToken => ({
+    type: SET_AUTH_TOKEN,
+    authToken
+});
+
+export const authSuccess = currentUser => ({
+    type: AUTH_SUCCESS,
+    currentUser
+});
+
+export const logOut = () => ({
+    type: LOG_OUT
+});
+
+export const signup = (user) => ({
+    type: SIGN_UP,
+    user
 });
 
 export const logIn = (user) => ({
     type: LOG_IN,
     user
-});
-
-export const logOut = () => ({
-    type: LOG_OUT
 });
