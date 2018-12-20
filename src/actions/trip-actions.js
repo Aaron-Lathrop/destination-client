b065007dc5ed0a48caca51f11d09c730a91962f4
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-20 14:51:29 -0700

    add action pseudocode

--- src/actions/trip-actions.js
+++ src/actions/trip-actions.js
@@ -2,3 +2,19 @@ export const ADD_TRIP = 'ADD_TRIP';
 export const GET_TRIPS = 'GET_TRIPS';
 export const UPDATE_TRIP = 'UPDATE_TRIP';
 export const DELETE_TRIP = 'DELETE_TRIP';
+
+export const addTrip = () => ({
+    type: ADD_TRIP 
+});
+
+export const getTrips = () => ({
+    type: GET_TRIPS
+});
+
+export const updateTrip = () => ({
+    type: UPDATE_TRIP
+});
+
+export const deleteTrip = () => ({
+    type: DELETE_TRIP
+});
--- src/actions/user-actions.js
+++ src/actions/user-actions.js
@@ -1,3 +1,20 @@
 export const CREATE_USER = 'CREATE_USER';
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
+
+export const createUser = (username, password, name, email) => ({
+    type: CREATE_USER,
+    username,
+    password,
+    name,
+    email
+});
+
+export const logIn = (user) => ({
+    type: LOG_IN,
+    user
+});
+
+export const logOut = () => ({
+    type: LOG_OUT
+});
