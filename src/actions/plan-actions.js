363e481c3f0abab8370366bd02aef51be0cb787b
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-20 14:41:59 -0700

    add action constants

---
+++ src/actions/plan-actions.js
@@ -0,0 +1,5 @@
+export const ADD_PLAN = 'ADD_PLAN';
+export const GET_PLANS = 'GET_PLANS';
+export const UPDATE_PLAN = 'UPDATE_PLAN';
+export const DELETE_PLAN = 'DELETE_PLAN';
+export const GET_WEATHER = 'GET_WEATHER';
---
+++ src/actions/user-actions.js
@@ -0,0 +1,3 @@
+export const CREATE_USER = 'CREATE_USER';
+export const LOG_IN = 'LOG_IN';
+export const LOG_OUT = 'LOG_OUT';
