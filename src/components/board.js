61a255b61d234da0b9120f424b52502d9004c221
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-20 17:28:14 -0700

    update nav component

--- src/components/board.js
+++ src/components/board.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
-import { Router } from 'react-router-dom';
 import './board.css';
 import Nav from './nav'
 
 export default class Board extends Component {
     render() {
         return (
-            
-                <Nav />
+            <Nav />
         );
     }
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './nav.css';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
-    return (
-        <div>
-            <nav role="navigation">
-                Nav
-                
-            </nav>
-        </div>
+    return (        
+        <Router>
+            <div>
+                <nav role="navigation">
+                    Nav
+                    <Link to={`/`}>Login</Link>
+                </nav>
+                <header role="banner">
+                    <h1>DestiNation</h1>
+                    <h2>because so much depends upon the weather</h2>
+                    <button>Plan Trip</button>
+                </header>
+            </div>
+        </Router>
     );
 }
