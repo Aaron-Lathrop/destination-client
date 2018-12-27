b92b361c6ea95933239d626c431a1e769d9ba37a
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-27 09:19:03 -0700

    added smoke tests for all components

--- src/App.test.js
+++ src/App.test.js
@@ -5,15 +5,8 @@ import { shallow } from 'enzyme';
 import App from './App';
 Enzyme.configure({adapter: new Adapter()});
 
-
-// import ReactDOM from 'react-dom';
-
-
 describe('<App />', () => {
   it('Renders without crashing', () => {
     shallow(<App />);
   });
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
 });
