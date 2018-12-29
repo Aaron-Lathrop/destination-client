6c8a2808fb1be3399aea974fddffe8d6bd730c98
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-28 18:37:28 -0700

    add getDates method

--- src/components/trip-form.js
+++ src/components/trip-form.js
@@ -5,6 +5,35 @@ export default function TripForm(props) {
 
   const trip = {};
 
+  function parseDate(date) {
+    const dateValues = {
+      month: new Date(date).getMonth() + 1,
+      day: new Date(date).getDate() + 1,
+      year: new Date(date).getFullYear(),
+      string: `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`
+    };
+    return dateValues;
+  }
+
+//Credit to John Hartsock on StackOverflow https://stackoverflow.com/questions/4413590/javascript-get-array-of-dates-between-2-dates for Date.prototype.addDays and function getDates()
+  Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+function getDates(startDate, stopDate) {
+    var dateArray = [];
+    var currentDate = startDate;
+    while (currentDate <= stopDate) {
+        dateArray.push(parseDate(new Date (currentDate)).string );
+        currentDate = currentDate.addDays(1);
+    }
+    return dateArray;
+}
+
+console.log(getDates(new Date("2018/12/28"), new Date("2019/01/12")));
+
   function onSubmit(e) {
     e.preventDefault();
     window.location = "/trips";
@@ -18,7 +47,8 @@ export default function TripForm(props) {
   function handleArrival(e) {
     trip.arrival = e.target.value;
     console.log(trip);
-    console.log(new Date(e.target.value));
+    console.log(`${new Date(e.target.value).getMonth() + 1}/${new Date(e.target.value).getDate() + 1}/${new Date(e.target.value).getFullYear()}`);
+    console.log(e.target.value instanceof Date, " expect false");
   }
 
   function handleReturn(e) {
