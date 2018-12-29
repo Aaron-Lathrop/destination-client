7e4d43fde4fd2a693251c143bfaf695cd0a31b24
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2018-12-29 15:26:20 -0700

    add null screen for /trips

--- src/components/trip-card.js
+++ src/components/trip-card.js
@@ -5,7 +5,6 @@ import { withRouter } from 'react-router-dom';
 
 import TripFormUpdate from './trip-form-update';
 import { setEditing } from '../actions';
-//import { Link } from 'react-router-dom';
 
 function TripCard(props) {
 
@@ -34,24 +33,43 @@ function TripCard(props) {
         );
     }
 
-    const trips = props.trips.map((trip, index) => (
-        <section key={trip.tripId} id={trip.tripId} >
-            <div className="trip">
-                <div className="trip-description">
-                    <p>Trip to {trip.destination}!</p>
-                    <p><span id="start">{trip.startDate}</span> to <span id="end">{trip.endDate}</span></p>
-                </div>
-                <div>
-                    <img 
-                    src={trip.icon} 
-                    alt={trip.destination} className="trip-img" />
-                </div>
-            </div>
-            {userInteractions(trip)}
-            {/* <button onClick={e => handleView(trip.tripId)}>View</button>
-            <button onClick={e => handleEditing()}>Update</button> */}
-        </section>
-    ));
+    function handleGetStarted() {
+        props.history.push('/dashboard');
+    }
+
+    const tripsValue = () => {
+
+        if(props.trips.length > 0) {
+            return (
+                props.trips.map((trip, index) => (
+                    <section key={trip.tripId} id={trip.tripId} >
+                        <div className="trip">
+                            <div className="trip-description">
+                                <p>Trip to {trip.destination}!</p>
+                                <p><span id="start">{trip.startDate}</span> to <span id="end">{trip.endDate}</span></p>
+                            </div>
+                            <div>
+                                <img 
+                                src={trip.icon} 
+                                alt={trip.destination} className="trip-img" />
+                            </div>
+                        </div>
+                        {userInteractions(trip)}
+                    </section>
+                ))
+            );
+        }
+
+        return (
+            <section>
+                <h1>You haven't created any trips yet.</h1>
+                <button onClick={e => handleGetStarted()}>Get Started</button>
+            </section>
+        );
+        
+    }
+    
+    const trips = tripsValue();
 
     return (
         <div className="sticky-footer">
