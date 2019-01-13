e3ae73090eb79719f3b0496179af89f50520a82d
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-01-13 10:28:32 -0700

    update action tests for changes in app design

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -1,14 +1,10 @@
 import {
-    SET_PLAN_CARDS,
     ADD_PLAN,
-    GET_PLANS,
     EDIT_PLANS,
-    CANCEL_EDIT_PLAN,
     UPDATE_PLAN,
     DELETE_PLAN,
-    GET_WEATHER,
     ADD_TRIP,
-    GET_TRIPS,
+    LOAD_TRIPS,
     UPDATE_TRIP,
     DELETE_TRIP,
     REQUEST,
@@ -19,42 +15,23 @@ import {
     SIGN_UP,
     LOG_IN,
     SET_EDITING,
-    setPlanCards,
     addPlan,
-    getPlans,
     editPlans,
-    cancelEditPlan,
     updatePlan,
     deletePlan,
-    getWeather,
     addTrip,
-    getTrips,
+    loadTrips,
     updateTrip,
     deleteTrip,
     request,
     authRequest,
     setAuthToken,
     authSuccess,
-    logOut,
+    logout,
     signup,
-    logIn,
+    login,
     setEditing
 } from './index';
-
-describe('setPlanCards', () => {
-    it('Should return the action', () => {
-        const planCards = [{
-            tripId: 54321,
-            plans: "this is a test plan",
-            date: "01/01/2019",
-            weather: "super cold",
-            index: 0
-         }]; 
-        const action = setPlanCards(planCards);
-        expect(action.type).toEqual(SET_PLAN_CARDS);
-        expect(action.planCards).toEqual(planCards);
-    });
-});
 
 describe('addPlan', () => {
     it('Should return the action', () => {
@@ -71,13 +48,6 @@ describe('addPlan', () => {
     });
 });
 
-describe('getPlans', () => {
-    it('Should return the action', () => {
-        const action = getPlans();
-        expect(action.type).toEqual(GET_PLANS);
-    });
-});
-
 describe('editPlans', () => {
     it('Should return the action', () => {
         const planCards = [{
@@ -89,23 +59,6 @@ describe('editPlans', () => {
          }];
         const action = editPlans(planCards);
         expect(action.type).toEqual(EDIT_PLANS);
-        expect(action.planCards).toEqual(planCards);
-    });
-});
-
-describe('cancelEditPlan', () => {
-    it('Should return the action', () => {
-        const planCards = [{
-            tripId: 54321,
-            plans: "this is a test plan",
-            date: "01/01/2019",
-            weather: "super cold",
-            index: 0
-         }];
-         const tripId = 54321;
-        const action = cancelEditPlan(tripId, planCards);
-        expect(action.type).toEqual(CANCEL_EDIT_PLAN);
-        expect(action.tripId).toEqual(tripId);
         expect(action.planCards).toEqual(planCards);
     });
 });
@@ -131,13 +84,6 @@ describe('deletePlan', () => {
     });
 });
 
-describe('getWeather', () => {
-    it('Should return the action', () => {
-        const action = getWeather();
-        expect(action.type).toEqual(GET_WEATHER);
-    });
-});
-
 describe('addTrip', () => {
     it('Should return the action', () => {
         const action = addTrip();
@@ -145,10 +91,10 @@ describe('addTrip', () => {
     });
 });
 
-describe('getTrips', () => {
+describe('loadTrips', () => {
     it('Should return the action', () => {
-        const action = getTrips();
-        expect(action.type).toEqual(GET_TRIPS);
+        const action = loadTrips();
+        expect(action.type).toEqual(LOAD_TRIPS);
     });
 });
 
@@ -195,26 +141,26 @@ describe('authSuccess', () => {
     });
 });
 
-describe('logOut', () => {
+describe('logout', () => {
     it('Should return the action', () => {
-        const action = logOut();
+        const action = logout();
         expect(action.type).toEqual(LOG_OUT);
     });
 });
 
-describe('signup', () => {
-    it('Should return the action', () => {
-        const action = signup();
-        expect(action.type).toEqual(SIGN_UP);
-    });
-});
+// describe('signup', () => {
+//     it('Should return the action', () => {
+//         const action = signup();
+//         expect(action.type).toEqual(SIGN_UP);
+//     });
+// });
 
-describe('logIn', () => {
-    it('Should return the action', () => {
-        const action = logIn();
-        expect(action.type).toEqual(LOG_IN);
-    });
-});
+// describe('login', () => {
+//     it('Should return the action', () => {
+//         const action = login();
+//         expect(action.type).toEqual(LOG_IN);
+//     });
+// });
 
 describe('setEditing', () => {
     it('Should return the action', () => {
