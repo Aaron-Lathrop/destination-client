5e0ff9c1162a62dae6810bd625af5e563a644fc5
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-01-23 18:07:49 -0700

    add normalize css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './normalize.css'
 import './App.css';
 import {connect} from 'react-redux';
 import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
