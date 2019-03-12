a953ef6d7e3cba22b1b4da2051083a74ae587e0d
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-03-12 10:23:22 -0600

    change API_BASE_URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'https://destino-simple-planning.herokuapp.com';
+    process.env.REACT_APP_API_BASE_URL || 'https://destino-planning.herokuapp.com';
