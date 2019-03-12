0ddce36a30fac50a75d042a2560619e3d367e738
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-03-12 10:45:12 -0600

    change production express default url

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ if(!dev) {
 
     app.use(express.static(path.resolve(__dirname, 'build')));
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 }
